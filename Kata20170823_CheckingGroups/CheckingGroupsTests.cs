41c925f399b22951818f3dbc5f07429a3f2f8e7d
cash <cash@cashwu.com>
2017-08-23 21:05:18 +0800

    input () test case

---
+++ Kata20170823_CheckingGroups/CheckingGroupsTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Kata20170823_CheckingGroups
+{
+    [TestClass]
+    public class CheckingGroupsTests
+    {
+        [TestMethod]
+        public void input_L_brackets_R_brackets_should_return_true()
+        {
+            CheckGroupsShouldBeTrue("()");
+        }
+
+        private static void CheckGroupsShouldBeTrue(string input)
+        {
+            var groups = new Groups();
+            var actual = groups.Check(input);
+            Assert.IsTrue(actual);
+        }
+    }
+
+    public class Groups
+    {
+        public bool Check(string input)
+        {
+            return true;
+        }
+    }
+}
